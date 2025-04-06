515d811fa4e8f5caee1814093224ff5330e3050d
Dikshit Yadav <2004dikshityadav@gmail.com>
2025-04-06 19:32:54 +0530

    Done

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
+
 //home 
 app.get('/', async (req, res) => {
   try {
